e0ece2faa1653c5f1811f8b63c07ff6db08013b2
John Doe <johne.doe@domain.tld>
2019-02-15 10:26:15 +0200

    ...

--- main.js
+++ main.js
@@ -1,50 +1,120 @@
+"use strict";
+
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu, shell} = require('electron');
+const fs = require('fs')
+const config = require('./config');
+const server = require('./server');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
+
+
+function createRootDir() {
+  if (!fs.existsSync(config.STORAGE_ROOT)) {
+      fs.mkdirSync(config.STORAGE_ROOT);
+  }
+  if (!fs.existsSync(config.IMAGES_PATH)) {
+      fs.mkdirSync(config.IMAGES_PATH);
+  }
+  if (!fs.existsSync(config.VIDEOS_PATH)) {
+      fs.mkdirSync(config.VIDEOS_PATH);
+  }
+}
 
 function createWindow () {
+
+  let url;
+
+  if (process.env.NODE_ENV === 'DEV') {
+    url = 'http://localhost:8080/';
+  } else {
+    url = `file://${process.cwd()}/dist/index.html`
+  }
+
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    acceptFirstMouse: true,
+    center: true,
+    fullscreenable: false,
+    resizable: false,
+    show: true,
+    autoHideMenuBar: true
+  })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL(url);
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
+
+  server.createServer();
+
+  loadMainMenu();
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
 
-// Quit when all windows are closed.
+
+app.on('ready', createWindow);
+
+
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow()
   }
-})
+});
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
+function loadMainMenu() {
+  let template = [{
+    label: config.APP_NAME,
+    submenu: [
+      { label: "About Application", click: () => { shell.openExternal(config.HOME_PAGE_URL) } },
+      { label: "Support", click: () => { shell.openExternal(config.GITHUB_URL_ISSUES) } },
+      { label: `Check for updates (current: v${config.APP_VERSION})`, click: () => { shell.openExternal(config.UPDATES_URL) } },
+      { type: "separator" },
+      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]
+  }];
+
+  if (process.platform === 'darwin') {
+    template = [{
+        label: config.APP_NAME,
+        submenu: [
+            { label: "About " +  config.APP_NAME, click: () => { shell.openExternal(config.HOME_PAGE_URL) } },
+            { label: "Support", click: () => { shell.openExternal(config.GITHUB_URL_ISSUES) } },
+            { label: `Check for updates (current: v${config.APP_VERSION})`, click: () => { shell.openExternal(config.UPDATES_URL) } },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+        ]}, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}
+    ];
+  }
+
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+}
--- renderer.js
+++ renderer.js
@@ -1,3 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const electron = window.require('electron');
+const ipcRenderer = electron.ipcRenderer;
+
+
+console.log('Renderer loaded');
